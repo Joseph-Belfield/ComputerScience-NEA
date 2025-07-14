03fa5df8fd173230e642d0df4d8716563743161a
Joseph <alexrules008@gmail.com>
2025-07-14 11:17:59 +0100

    initializing file structure

---
+++ main.py
@@ -0,0 +1,3 @@
+# Just use functions from Python Work
+
+import python_source as py_source
---
+++ python_source/__init__.py
@@ -0,0 +1,3 @@
+# init files allow functions to be taken outside of folders into other python files
+
+from . import *     # 'import *' imports all folders and files in this directory
