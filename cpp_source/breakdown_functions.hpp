790adefa47ce7f2cf5a22784787bff2ca1c12b92
Joseph <alexrules008@gmail.com>
2025-07-15 08:54:29 +0100

    reorganized stupid namespace std placement

--- cpp_source/breakdown_functions.hpp
+++ cpp_source/breakdown_functions.hpp
@@ -2,11 +2,15 @@
 
 #include <regex>            // Enables RegEx library.
 #include <string>           // Enables strings.
-using namespace std;        // Strings are cool.
 
 #include <map>              // Maps act like dicts. Will be used for storing data about elements.
 
 #include "element_definitions.h"        // A set of useful #defines. Subs in integer values for element names, etc. 
+
+using namespace std;        // Do not have to call STD every time! 
+
+
+
 
 int* spine_regex() {
     int num_regexMatch = 1; //placeholder
