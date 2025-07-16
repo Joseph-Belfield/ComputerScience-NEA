83035f20312f8861964f6494aed82a52ba4dd815
Joseph <alexrules008@gmail.com>
2025-07-16 10:21:26 +0100

    updated gitignore to remove compiler files

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <string>  // I need strings I like strings
+#include <iostream>         // 'cout' and such
 using namespace std;        // Makes strings work!
 
 // Includes the file with all the functions. Can just be used as normal.
@@ -7,8 +8,17 @@ using namespace std;        // Makes strings work!
 // Python will run C++ as executable.
 
 // Run previous functions in main. Cannot make functions in function unlike Python.
-int main(int argc, char** argv) {
+int main(int argc, char* argv[]) {
 
+// print all command line arguments
+    std::cout << "name of program: " << argv[0] << '\n' ;
+
+    if( argc > 1 )
+    {
+        std::cout << "there are " << argc-1 << " (more) arguments, they are:\n" ;
+
+        std::copy( argv+1, argv+argc, std::ostream_iterator<const char*>( std::cout, "\n" ) ) ;
+    }
 
     return 0;
 }
