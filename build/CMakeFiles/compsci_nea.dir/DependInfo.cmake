52bf5491f87e0d7b0aad8acc2e50d5132a7538ce
Joseph <alexrules008@gmail.com>
2025-12-31 19:37:25 +0000

    attempted to set up cmake with imgui and sdl. cmakelists file taken from guide. main file taken as example from guide - will be changed later (only for testing purposes rn)

---
+++ build/CMakeFiles/compsci_nea.dir/DependInfo.cmake
@@ -0,0 +1,30 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/josephbelfield/Code/ComputerScience-NEA/lib/imgui/backends/imgui_impl_sdl3.cpp" "CMakeFiles/compsci_nea.dir/lib/imgui/backends/imgui_impl_sdl3.cpp.o" "gcc" "CMakeFiles/compsci_nea.dir/lib/imgui/backends/imgui_impl_sdl3.cpp.o.d"
+  "/Users/josephbelfield/Code/ComputerScience-NEA/lib/imgui/backends/imgui_impl_sdlrenderer3.cpp" "CMakeFiles/compsci_nea.dir/lib/imgui/backends/imgui_impl_sdlrenderer3.cpp.o" "gcc" "CMakeFiles/compsci_nea.dir/lib/imgui/backends/imgui_impl_sdlrenderer3.cpp.o.d"
+  "/Users/josephbelfield/Code/ComputerScience-NEA/lib/imgui/imgui.cpp" "CMakeFiles/compsci_nea.dir/lib/imgui/imgui.cpp.o" "gcc" "CMakeFiles/compsci_nea.dir/lib/imgui/imgui.cpp.o.d"
+  "/Users/josephbelfield/Code/ComputerScience-NEA/lib/imgui/imgui_demo.cpp" "CMakeFiles/compsci_nea.dir/lib/imgui/imgui_demo.cpp.o" "gcc" "CMakeFiles/compsci_nea.dir/lib/imgui/imgui_demo.cpp.o.d"
+  "/Users/josephbelfield/Code/ComputerScience-NEA/lib/imgui/imgui_draw.cpp" "CMakeFiles/compsci_nea.dir/lib/imgui/imgui_draw.cpp.o" "gcc" "CMakeFiles/compsci_nea.dir/lib/imgui/imgui_draw.cpp.o.d"
+  "/Users/josephbelfield/Code/ComputerScience-NEA/lib/imgui/imgui_tables.cpp" "CMakeFiles/compsci_nea.dir/lib/imgui/imgui_tables.cpp.o" "gcc" "CMakeFiles/compsci_nea.dir/lib/imgui/imgui_tables.cpp.o.d"
+  "/Users/josephbelfield/Code/ComputerScience-NEA/lib/imgui/imgui_widgets.cpp" "CMakeFiles/compsci_nea.dir/lib/imgui/imgui_widgets.cpp.o" "gcc" "CMakeFiles/compsci_nea.dir/lib/imgui/imgui_widgets.cpp.o.d"
+  "/Users/josephbelfield/Code/ComputerScience-NEA/src/main.cpp" "CMakeFiles/compsci_nea.dir/src/main.cpp.o" "gcc" "CMakeFiles/compsci_nea.dir/src/main.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
