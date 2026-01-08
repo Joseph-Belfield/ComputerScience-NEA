e5df04cdb06c5366c5e5577d699b12211d9338b8
Joseph <alexrules008@gmail.com>
2026-01-08 21:42:20 +0000

    added the ebo and now program draws quad

--- src/main.cpp
+++ src/main.cpp
@@ -80,9 +80,9 @@ namespace global
   bool flag_mainLoop = true;
 
   // unsigned ints as identifiers for the objects (because C-based language)
-  GLuint vertexArrayObject;
-  GLuint vertexBufferObject_position;
-  GLuint vertexBufferObject_color;
+  GLuint vertexArrayObject = 0;
+  GLuint vertexBufferObject = 0;
+  GLuint indexBufferObject = 0;
 
   // unique ID for the graphics pipeline
   GLuint shaderProgram = 0;
@@ -228,79 +228,89 @@ void init_ImGui()
 void vertex_specification()
 {
   // use of GLfloat as it is more cross-platform (likely won't matter but best practice)
-  const std::vector<GLfloat> vertexPosition   // lives on CPU
+  const std::vector<GLfloat> vertexData   // lives on CPU
   {
-    //  x      y      z
-      -0.8f, -0.8f,  0.0f,    // vertex 1
-       0.8f, -0.8f,  0.0f,    // vertex 2
-       0.0f,  0.8f,  0.0f     // vertex 3
+    -0.5f, -0.5f,  0.0f,    // vertex 1
+     1.0f,  0.0f,  0.0f,    // color
+
+     0.5f, -0.5f,  0.0f,    // vertex 2
+     0.0f,  1.0f,  0.0f,    // color
+
+    -0.5f,  0.5f,  0.0f,    // vertex 3
+     0.0f,  0.0f,  1.0f,    // color   
+
+     0.5f,  0.5f,  0.0f,    // vertex 4 
+     1.0f,  1.0f,  1.0f     // color
   };
 
-  const std::vector<GLfloat> vertexColor  
-  {
-    //  r      g      b
-       1.0f,  0.0f,  0.0f,    // vertex 1
-       0.0f,  1.0f,  0.0f,    // vertex 2
-       0.0f,  0.0f,  1.0f     // vertex 3
-  };
 
   // generate Vertex Array Objects  
   glGenVertexArrays(1, &global::vertexArrayObject);             // creates an array to hold vertex data (called vertexArrayObject)
   glBindVertexArray(global::vertexArrayObject);                 // selects the array as current
 
   // generate Vertex Buffer Object for position
-  glGenBuffers(1, &global::vertexBufferObject_position);                 // generates buffer
-  glBindBuffer(GL_ARRAY_BUFFER, global::vertexBufferObject_position);    // sets buffer as current, specifies target
+  glGenBuffers(1, &global::vertexBufferObject);                 // generates buffer
+  glBindBuffer(GL_ARRAY_BUFFER, global::vertexBufferObject);    // sets buffer as current, specifies target
   glBufferData
   (
     GL_ARRAY_BUFFER,                           // specifies target
-    vertexPosition.size() * sizeof(GLfloat),  // finds the size (in bytes) of vertex data
-    vertexPosition.data(),                    // pointer to the array holding the data of the vector
+    vertexData.size() * sizeof(GLfloat),       // finds the size (in bytes) of vertex data
+    vertexData.data(),                         // pointer to the array holding the data of the vector
     GL_STATIC_DRAW                             // sets intentions with data
-  );                           
+  );                          
 
-  // setup VAO
-  glEnableVertexAttribArray(0); // enables the 0th attribute
-  glVertexAttribPointer
-  (
-    0,          // index into vecter
-    3,          // pieces of data (per vertex - x, y, z)
-    GL_FLOAT,   // data type
-    GL_FALSE,   // normalized?
-    0,          // stride (byte offset) between data types (eg: position, color)           
-    (void*)0    // pointer for offset
-  );
 
- // create a VBO for vertex colors
-  glGenBuffers(1, &global::vertexBufferObject_color);
-  glBindBuffer(GL_ARRAY_BUFFER, global::vertexBufferObject_color);
+  // data about the order vertices should be renderd in
+  const std::vector<GLuint> indexBufferData
+  {
+    2, 0, 1,      // triangle 1
+    3, 2, 1       // triangle 2
+  };
+
+  // set up Element/Index Buffer Object (EBO / IBO) - holds the index for the order in which vertices are drawn
+  glGenBuffers(1,&global::indexBufferObject);                         // generate EBO
+  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, global::indexBufferObject);   // sets buffer type as element buffer
   glBufferData
   (
-    GL_ARRAY_BUFFER,
-    vertexColor.size() * sizeof(GLfloat),
-    vertexColor.data(),
-    GL_STATIC_DRAW
+    GL_ELEMENT_ARRAY_BUFFER,                        // target
+    indexBufferData.size() * sizeof(GLuint),        // size
+    indexBufferData.data(),                         // data
+    GL_STATIC_DRAW                                  // usage
   );
 
-  // linking attributes for colors in VAO
-  glEnableVertexAttribArray(1);
+
+
+  // setup position VAO
+  glEnableVertexAttribArray(0); // enables the 0th attribute - AKA. this is the first VAO
   glVertexAttribPointer
   (
-    1,
-    3,          // r g b
-    GL_FLOAT,
-    GL_FALSE,
-    0,
-    (void*)0
+    0,                        // index into vector of VAOs
+    3,                        // pieces of data (per vertex: x, y, z)
+    GL_FLOAT,                 // data type
+    GL_FALSE,                 // normalized?
+    sizeof(GL_FLOAT) * 6,     // stride (no. of bytes) to jump from first (type) data of v1 to first (type) data of v2, etc         
+    (GLvoid*)0                // pointer for offset - irrelivent as position data is in first slot
   );
+
+  // setup color VAO
+  glEnableVertexAttribArray(1); // enables the 1st attribute - AKA. this is the second VAO
+  glVertexAttribPointer
+  (
+    1,                                 // index into vector of VAOs
+    3,                                 // pieces of data (per vertex: r, g, b)
+    GL_FLOAT,                          // data type
+    GL_FALSE,                          // normalized?
+    sizeof(GL_FLOAT) * 6,              // stride (byte offset) between firsts of same data       
+    (GLvoid*)(sizeof(GL_FLOAT) * 3)    // pointer for offset - starting position for first of that data type (address)
+  );
+
 
   // cleanup VAO
   glBindVertexArray(0);                 // unbind currently bound VAO
 
   // disable any attributes previously opened in our vertex attribute array
   glDisableVertexAttribArray(0);        // position
-  glDisableVertexAttribArray(1);        // color
-
+  glDisableVertexAttribArray(1);
 }
 
 
@@ -472,14 +482,15 @@ void draw_OpenGL()
 {
   // choose VAO and VBO
   glBindVertexArray(global::vertexArrayObject);
-  glBindBuffer(GL_ARRAY_BUFFER, global::vertexBufferObject_position);
+  glBindBuffer(GL_ARRAY_BUFFER, global::vertexBufferObject);
 
   // draw
-  glDrawArrays
+  glDrawElements
   (
     GL_TRIANGLES,         // shape
-    0,                    // starting index for first vertex
-    3                     // no. of vertices
+    6,                    // number of vertices drawn to (count repeats)
+    GL_UNSIGNED_INT,      // data type
+    0                     // offset into index array for first element (triangle vertex order)
   );
 }
 
