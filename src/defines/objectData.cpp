de9d1141a386e8e2101a02f9674eb5e082b8631d
Joseph <alexrules008@gmail.com>
2026-01-27 15:39:45 +0000

    turned vertex spec back into a function so that the object data source file is shorter and more readible

--- src/defines/objectData.cpp
+++ src/defines/objectData.cpp
@@ -76,6 +76,66 @@ void Object::draw_lines(GLuint shaderProgram)
     glBindVertexArray(0);
 }
 
+void vertex_specification(meshData &mesh)
+{
+	// generate Vertex Array Objects 
+	glGenVertexArrays(1, &(mesh.vertexArrayObject));           // creates an array to hold vertex data (called vertexArrayObject)
+	glBindVertexArray(mesh.vertexArrayObject);                 // selects the array as current
+
+
+	// generate Vertex Buffer Object for position
+	glGenBuffers(1, &(mesh.vertexBufferObject));               // generates buffer
+	glBindBuffer(GL_ARRAY_BUFFER, mesh.vertexBufferObject);    // sets buffer as current, specifies target
+	glBufferData
+	(
+		GL_ARRAY_BUFFER,                          							// specifies target
+		mesh.vertexData.size() * sizeof(GLfloat),      	 				   	// finds the size (in bytes) of vertex data
+		mesh.vertexData.data(),                       		  			  	// pointer to the array holding the data of the vector
+		GL_STATIC_DRAW                             							// sets intentions with data
+	);
+
+	// set up Element/Index Buffer Object (EBO / IBO) - holds the index for the order in which vertices are drawn
+	glGenBuffers(1,&(mesh.indexBufferObject));                         // generate EBO
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, mesh.indexBufferObject);   // sets buffer type as element buffer
+	glBufferData
+	(
+		GL_ELEMENT_ARRAY_BUFFER,                        		// target
+		mesh.indexData.size() * sizeof(GLuint),    		   		// size
+		mesh.indexData.data(),                        			// data
+		GL_STATIC_DRAW                                			// usage
+	);
+
+	
+	// setup position VAO
+	glEnableVertexAttribArray(0); // enables the 0th attribute - AKA. this is the first VAO
+	glVertexAttribPointer
+	(
+		0,                        // index into vector of VAOs
+		3,                        // pieces of data (per vertex: x, y, z)
+		GL_FLOAT,                 // data type
+		GL_FALSE,                 // normalized?
+		sizeof(GLfloat) * 7,      // stride (no. of bytes) to jump from first (type) data of v1 to first (type) data of v2, etc         
+		(GLvoid*)0                // pointer for offset - irrelivent as position data is in first slot
+	);
+
+
+	// setup color VAO
+	glEnableVertexAttribArray(1); // enables the 1st attribute - AKA. this is the second VAO
+	glVertexAttribPointer
+	(
+		1,                                 // index into vector of VAOs
+		4,                                 // pieces of data (per vertex: r, g, b, a)
+		GL_FLOAT,                          // data type
+		GL_FALSE,                          // normalized?
+		sizeof(GLfloat) * 7,               // stride (byte offset) between firsts of same data (ie: between r1 and r2)       
+		(GLvoid*)(sizeof(GLfloat) * 3)     // pointer for offset - starting position for first of that data type (address)
+	);
+
+	// cleanup
+	glBindVertexArray(0);                		// unbind currently bound VAO
+	glBindBuffer(GL_ARRAY_BUFFER, 0);     		// unbind currently bound VBO
+}
+
 // calculates the coordinates of all points of a sphere object
 std::vector<glm::vec3> calculateSphereVertices(const float radius, const GLuint stackCount, const GLuint sectorCount)
 {	
@@ -173,71 +233,12 @@ Sphere::Sphere(const GLfloat radius, const GLuint stacks, const GLuint sectors, 
 		mesh.vertexData.push_back(initColor.b);			// B
 		mesh.vertexData.push_back(initColor.a);				// A
 	}
-	
-
-	// generate Vertex Array Objects 
-	glGenVertexArrays(1, &(mesh.vertexArrayObject));           // creates an array to hold vertex data (called vertexArrayObject)
-	glBindVertexArray(mesh.vertexArrayObject);                 // selects the array as current
-
-
-	// generate Vertex Buffer Object for position
-	glGenBuffers(1, &(mesh.vertexBufferObject));               // generates buffer
-	glBindBuffer(GL_ARRAY_BUFFER, mesh.vertexBufferObject);    // sets buffer as current, specifies target
-	glBufferData
-	(
-		GL_ARRAY_BUFFER,                          							// specifies target
-		mesh.vertexData.size() * sizeof(GLfloat),      	 				   	// finds the size (in bytes) of vertex data
-		mesh.vertexData.data(),                       		  			  	// pointer to the array holding the data of the vector
-		GL_STATIC_DRAW                             							// sets intentions with data
-	);
-	
 
 	// fills indexData with correct index information
 	calculateSphereIndexData(mesh.indexData, stacks, sectors);
 
-
-	// set up Element/Index Buffer Object (EBO / IBO) - holds the index for the order in which vertices are drawn
-	glGenBuffers(1,&(mesh.indexBufferObject));                         // generate EBO
-	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, mesh.indexBufferObject);   // sets buffer type as element buffer
-	glBufferData
-	(
-		GL_ELEMENT_ARRAY_BUFFER,                        		// target
-		mesh.indexData.size() * sizeof(GLuint),    		   		// size
-		mesh.indexData.data(),                        			// data
-		GL_STATIC_DRAW                                			// usage
-	);
-
-	
-	// setup position VAO
-	glEnableVertexAttribArray(0); // enables the 0th attribute - AKA. this is the first VAO
-	glVertexAttribPointer
-	(
-		0,                        // index into vector of VAOs
-		3,                        // pieces of data (per vertex: x, y, z)
-		GL_FLOAT,                 // data type
-		GL_FALSE,                 // normalized?
-		sizeof(GLfloat) * 7,      // stride (no. of bytes) to jump from first (type) data of v1 to first (type) data of v2, etc         
-		(GLvoid*)0                // pointer for offset - irrelivent as position data is in first slot
-	);
-
-
-	// setup color VAO
-	glEnableVertexAttribArray(1); // enables the 1st attribute - AKA. this is the second VAO
-	glVertexAttribPointer
-	(
-		1,                                 // index into vector of VAOs
-		4,                                 // pieces of data (per vertex: r, g, b, a)
-		GL_FLOAT,                          // data type
-		GL_FALSE,                          // normalized?
-		sizeof(GLfloat) * 7,               // stride (byte offset) between firsts of same data (ie: between r1 and r2)       
-		(GLvoid*)(sizeof(GLfloat) * 3)     // pointer for offset - starting position for first of that data type (address)
-	);
-
-
-	// cleanup
-	glBindVertexArray(0);                		// unbind currently bound VAO
-	glBindBuffer(GL_ARRAY_BUFFER, 0);     		// unbind currently bound VBO
-
+	// sets up VAO, VBO and IBO
+	vertex_specification(mesh);
 
 	// set up uniforms
 	uniform.uDisplacement = initLocation;
@@ -307,66 +308,9 @@ ReferencePlane::ReferencePlane(GLfloat initHeight, const glm::vec4 initColor, GL
 		mesh.vertexData.push_back(initColor.a);			// A
 	}
 
-	// generate Vertex Array Objects 
-	glGenVertexArrays(1, &(mesh.vertexArrayObject));           // creates an array to hold vertex data (called vertexArrayObject)
-	glBindVertexArray(mesh.vertexArrayObject);                 // selects the array as current
-
-
-	// generate Vertex Buffer Object for position
-	glGenBuffers(1, &(mesh.vertexBufferObject));               // generates buffer
-	glBindBuffer(GL_ARRAY_BUFFER, mesh.vertexBufferObject);    // sets buffer as current, specifies target
-	glBufferData
-	(
-		GL_ARRAY_BUFFER,                          							// specifies target
-		mesh.vertexData.size() * sizeof(GLfloat),      	 				   	// finds the size (in bytes) of vertex data
-		mesh.vertexData.data(),                       		  			  	// pointer to the array holding the data of the vector
-		GL_STATIC_DRAW                             							// sets intentions with data
-	);
-
-	mesh.indexData.reserve(stripCount * stripCount);
 	calculateReferencePlaneIndexData(mesh.indexData, stripCount);
 
-	// set up Element/Index Buffer Object (EBO / IBO) - holds the index for the order in which vertices are drawn
-	glGenBuffers(1,&(mesh.indexBufferObject));                         // generate EBO
-	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, mesh.indexBufferObject);   // sets buffer type as element buffer
-	glBufferData
-	(
-		GL_ELEMENT_ARRAY_BUFFER,                        		// target
-		mesh.indexData.size() * sizeof(GLuint),    		   		// size
-		mesh.indexData.data(),                        			// data
-		GL_STATIC_DRAW                                			// usage
-	);
-
-	
-	// setup position VAO
-	glEnableVertexAttribArray(0); // enables the 0th attribute - AKA. this is the first VAO
-	glVertexAttribPointer
-	(
-		0,                        // index into vector of VAOs
-		3,                        // pieces of data (per vertex: x, y, z)
-		GL_FLOAT,                 // data type
-		GL_FALSE,                 // normalized?
-		sizeof(GLfloat) * 7,      // stride (no. of bytes) to jump from first (type) data of v1 to first (type) data of v2, etc         
-		(GLvoid*)0                // pointer for offset - irrelivent as position data is in first slot
-	);
-
-
-	// setup color VAO
-	glEnableVertexAttribArray(1); // enables the 1st attribute - AKA. this is the second VAO
-	glVertexAttribPointer
-	(
-		1,                                 // index into vector of VAOs
-		4,                                 // pieces of data (per vertex: r, g, b, a)
-		GL_FLOAT,                          // data type
-		GL_FALSE,                          // normalized?
-		sizeof(GLfloat) * 7,               // stride (byte offset) between firsts of same data (ie: between r1 and r2)       
-		(GLvoid*)(sizeof(GLfloat) * 3)     // pointer for offset - starting position for first of that data type (address)
-	);
-
-	// cleanup
-	glBindVertexArray(0);                		// unbind currently bound VAO
-	glBindBuffer(GL_ARRAY_BUFFER, 0);     		// unbind currently bound VBO
-
+	vertex_specification(mesh);
 
 	// set up uniforms
 	uniform.uDisplacement = glm::vec3(0.0f, initHeight, 0.0f);
@@ -491,66 +435,12 @@ Cylinder::Cylinder(const GLfloat radius, const GLfloat height, const GLuint sect
 		mesh.vertexData.push_back(initColor.a);				// A
 	}
 
-	// generate Vertex Array Objects 
-	glGenVertexArrays(1, &(mesh.vertexArrayObject));           // creates an array to hold vertex data (called vertexArrayObject)
-	glBindVertexArray(mesh.vertexArrayObject);                 // selects the array as current
-
-
-	// generate Vertex Buffer Object for position
-	glGenBuffers(1, &(mesh.vertexBufferObject));               // generates buffer
-	glBindBuffer(GL_ARRAY_BUFFER, mesh.vertexBufferObject);    // sets buffer as current, specifies target
-	glBufferData
-	(
-		GL_ARRAY_BUFFER,                          							// specifies target
-		mesh.vertexData.size() * sizeof(GLfloat),      	 				   	// finds the size (in bytes) of vertex data
-		mesh.vertexData.data(),                       		  			  	// pointer to the array holding the data of the vector
-		GL_STATIC_DRAW                             							// sets intentions with data
-	);
 
 	// calculate index data for cylinder
 	calculateCylinderIndexData(mesh.indexData, sectorCount);
 
-	// set up Element/Index Buffer Object (EBO / IBO) - holds the index for the order in which vertices are drawn
-	glGenBuffers(1,&(mesh.indexBufferObject));                         // generate EBO
-	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, mesh.indexBufferObject);   // sets buffer type as element buffer
-	glBufferData
-	(
-		GL_ELEMENT_ARRAY_BUFFER,                        		// target
-		mesh.indexData.size() * sizeof(GLuint),    		   		// size
-		mesh.indexData.data(),                        			// data
-		GL_STATIC_DRAW                                			// usage
-	);
-
-	
-	// setup position VAO
-	glEnableVertexAttribArray(0); // enables the 0th attribute - AKA. this is the first VAO
-	glVertexAttribPointer
-	(
-		0,                        // index into vector of VAOs
-		3,                        // pieces of data (per vertex: x, y, z)
-		GL_FLOAT,                 // data type
-		GL_FALSE,                 // normalized?
-		sizeof(GLfloat) * 7,      // stride (no. of bytes) to jump from first (type) data of v1 to first (type) data of v2, etc         
-		(GLvoid*)0                // pointer for offset - irrelivent as position data is in first slot
-	);
-
-
-	// setup color VAO
-	glEnableVertexAttribArray(1); // enables the 1st attribute - AKA. this is the second VAO
-	glVertexAttribPointer
-	(
-		1,                                 // index into vector of VAOs
-		4,                                 // pieces of data (per vertex: r, g, b, a)
-		GL_FLOAT,                          // data type
-		GL_FALSE,                          // normalized?
-		sizeof(GLfloat) * 7,               // stride (byte offset) between firsts of same data (ie: between r1 and r2)       
-		(GLvoid*)(sizeof(GLfloat) * 3)     // pointer for offset - starting position for first of that data type (address)
-	);
-
-	// cleanup
-	glBindVertexArray(0);                		// unbind currently bound VAO
-	glBindBuffer(GL_ARRAY_BUFFER, 0);     		// unbind currently bound VBO
-
+	// sets up VAO, VBO and IBO
+	vertex_specification(mesh);
 
 	// set up uniforms
 	uniform.uDisplacement = initLocation;
